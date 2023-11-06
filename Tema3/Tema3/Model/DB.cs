3837bca338fc223b87e9edd398d15e1697ce8004
florinmitrana <128522815+florinmitrana@users.noreply.github.com>
2023-11-06 17:13:02 +0200

    Add files via upload

---
+++ Tema3/Tema3/Model/DB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tema3.Model
+{
+    public class DB : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+
+        public DB(DbContextOptions options) : base(options) { 
+        
+        }
+    }
+}
